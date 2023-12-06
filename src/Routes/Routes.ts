b764ca54a11e8e09bf589cc44176fcf66770ac74
Mashipro <captain.neon99@google.com>
2023-12-06 17:46:50 +0700

    remove static extension url

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -3,9 +3,17 @@ import path from "path";
 
 const RootRouter = Router();
 
-RootRouter.use("/static", express.static(path.join(__dirname, "/Static")));
+//Return static file json from static folder
+RootRouter.use(
+  "/static",
+  express.static(path.join(__dirname, "/Static"), {
+    index: false,
+    extensions: ["json"],
+  })
+);
 
 RootRouter.get("/", (req, res) => {
-  res.send(__dirname);
+  res.send(`Server is running..... Root: ${__dirname}`);
 });
+
 export default RootRouter;
