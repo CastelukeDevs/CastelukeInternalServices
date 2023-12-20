172cfc5534ebd41e097b4b49a0201f820a6a5b52
Mashipro <captain.neon99@google.com>
2023-12-20 15:58:39 +0700

    Refactor: Currency list from apps

--- src/Projects/Monst/Types/CurrencyTypes.ts
+++ src/Projects/Monst/Types/CurrencyTypes.ts
@@ -1,6 +1,17 @@
-export type ICurrencyTypes = {
-  currency: string;
-  abbreviation: string;
-  sign?: string;
-  id: string;
-};
+// export type ICurrencyTypes = {
+//   currency: string;
+//   abbreviation: string;
+//   sign?: string;
+// };
+
+const CurrencyList = [
+  { abbreviation: "IDR", currency: "Indonesian Rupiah", sign: "Rp" },
+  { abbreviation: "USD", currency: "US Dollar", sign: "$" },
+  { abbreviation: "EUR", currency: "European Euro", sign: "€" },
+  { abbreviation: "JPY", currency: "Japanese Yen", sign: "¥" },
+  { abbreviation: "SGD", currency: "Singaporean Dollar", sign: "$" },
+  { abbreviation: "SAR", currency: "Arabian Riyal", sign: "﷼" },
+  { abbreviation: "CNY", currency: "Chinese Yuan", sign: "¥" },
+] as const;
+
+export type ICurrencyTypes = (typeof CurrencyList)[number]["abbreviation"];
