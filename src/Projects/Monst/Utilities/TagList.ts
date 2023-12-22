afe0214e4a2e5c3369257f864bca5bd5d8b3e957
Mashipro <captain.neon99@google.com>
2023-12-22 18:09:38 +0700

    New: Pre-defined lists for currenct, category, and tags

---
+++ src/Projects/Monst/Utilities/TagList.ts
@@ -0,0 +1,6 @@
+export const TagList = [
+  { tags: "Yearly", definition: "Periodic", index: 0 },
+  { tags: "Monthly", definition: "Periodic", index: 0 },
+  { tags: "Weekly", definition: "Periodic", index: 0 },
+  { tags: "Daily", definition: "Periodic", index: 0 },
+] as const;
