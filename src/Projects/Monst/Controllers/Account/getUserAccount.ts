65f1c07b6cb4eaefab33a86969d7a73d93d646e9
Mashipro <captain.neon99@google.com>
2023-12-26 14:54:38 +0700

    Change: Omit array of object to be returned to user

--- src/Projects/Monst/Controllers/Account/getUserAccount.ts
+++ src/Projects/Monst/Controllers/Account/getUserAccount.ts
@@ -1,13 +1,21 @@
 import AccountModel from "@Projects/Monst/Models/AccountModel";
+import { omitObject } from "@Utilities/PickObject";
 import StatusCode from "@Utilities/StatusCode";
 import { Request, Response } from "express";
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 
 const getUserAccount = async (req: Request, res: Response) => {
   const tokenData: DecodedIdToken = res.locals.authData!;
-  const userBalance = await AccountModel.findById(tokenData.uid);
+  const userAccount = await AccountModel.findById(tokenData.uid);
+  console.log("get user account", userAccount);
 
-  if (userBalance) return res.send(userBalance);
+  if (userAccount) {
+    const dataPayload = omitObject(userAccount.toObject(), [
+      "transaction",
+      "wallet",
+    ]);
+    return res.send(dataPayload);
+  }
 
   res.status(StatusCode.notFound).send({
     message: "User account not found",
